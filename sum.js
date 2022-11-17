823a349ecdbced951fc3dfdca1e259f95c12e95f
sqz777 <icelffs@gmail.com>
2022-11-17 14:07:16 +0800

    proj init ocmplete

---
+++ sum.js
@@ -0,0 +1,12 @@
+/**
+ * Add two numbers.
+ * @param {number} a number 1
+ * @param {number} b number 2
+ * @return {number} The sum of the two numbers.
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
+
